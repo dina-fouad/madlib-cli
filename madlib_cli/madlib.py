c45e4d87b2ea0fd977138486a771adf626df30e8
dina-fouad <dina_albarghouthi@yahoo.com>
2021-07-27 22:29:42 +0300

    lab3

---
+++ madlib_cli/madlib.py
@@ -0,0 +1,32 @@
+def welcome_message():
+  print("""
+  **   Welcome User!   **
+  
+  A number of questions will be asked. 
+  Please answer all questions. 
+  """)
+
+
+
+
+def read_template(filepath: str) -> str:
+    if filepath!="assets/dark_and_stormy_night_template.txt":
+        raise FileNotFoundError('no path found')
+    file_content=''
+    with open(filepath, 'r') as file:
+   
+        file_content = file.read()
+    return file_content.strip()
+y=read_template('assets/dark_and_stormy_night_template.txt')
+print(y)    
+
+
+
+
+
+
+
+
+
+
+      
